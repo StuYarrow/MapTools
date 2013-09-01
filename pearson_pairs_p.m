81a9a00c80e8ba225a344f93d82377712d29d251
Stuart Yarrow <stu@outpost.org.uk>
2013-09-01 14:59:49 -0700

    Removing old functions.

--- pearson_pairs_p.m
+++
@@ -1,5 +0,0 @@
-function [r, p] = pearson_pairs_p(varargin)
-
-[r, p] = corr_pairs_p('pearson', varargin{:});
-
-end
--- spearman_pairs_p.m
+++
@@ -1,5 +0,0 @@
-function [r, p] = spearman_pairs_p(varargin)
-
-[r, p] = corr_pairs_p('spearman', varargin{:});
-
-end
